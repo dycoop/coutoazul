cb1c1fd2b7b25aa4bf5211016857c845b4d656d6
Olexey <olexiy@ua.pt>
2014-07-21 19:27:55 +0100

    coutoazul
    novo projeto

---
+++ Config/database.php
@@ -0,0 +1,13 @@
+<?php
+class DATABASE_CONFIG {
+
+	public $default = array(
+		'datasource' => 'Database/Sqlite',
+		'persistent' => false,
+		'host' => 'localhost',
+		'login' => 'root',
+		'password' => 'root',
+		'database' => 'coutoazul',
+		'encoding' => 'utf8'
+	);
+}
